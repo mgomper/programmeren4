945fe694b49cc2daf6b2df4ca3f5fe976f0633a8
mgomper <mgomper@avans.nl>
2017-05-07 18:35:22 +0200

    server.js gebruikt nu routes, routes toegevoegd

--- programmeren4/MyFirstMochaProject/server.js
+++ programmeren4/MyFirstMochaProject/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.all('*', function(req, res){
-  res.send('Er is een request verstuurd.');
-});
+app.all('*', function(request, response, next) {
+ console.log(request.method + " " + request.url);
+ next();
+})
+
+app.use('/api/v1', require('./routes/routes_api_v1'));
 
 app.listen(8080, function() {
     console.log('Server app is listening on port 8080');
