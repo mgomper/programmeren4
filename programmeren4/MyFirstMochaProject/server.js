8f888432d1fbd511b0b43cd8952678c9eaf4fed8
mgomper <mgomper@avans.nl>
2017-05-07 18:16:19 +0200

    config.json toegevoegd

--- programmeren4/MyFirstMochaProject/server.js
+++ programmeren4/MyFirstMochaProject/server.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.all('*', function(req, res){
   res.send('Er is een request verstuurd.');
-})
+});
 
 app.listen(8080, function() {
     console.log('Server app is listening on port 8080');
-  })
+  });
